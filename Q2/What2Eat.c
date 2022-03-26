6f7de9a4e10577241ea945f1612bf50c143c8c7a
musaberkay <musa.kocabasoglu@ozu.edu.tr>
2022-03-26 13:52:42 +0300

    Round 2 in q2 completed.

--- Q2/What2Eat.c
+++ Q2/What2Eat.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define THRESHOLD 10
+#define THRESHOLD 8
 
 int main(){
-	int count;
-	char foodArr[5][15]={"Kebab","Burger","Pasta","Lahmacun","Salad or Others"};
+	int countOfPeople;
+	char foodArr[5][15]={"Kebab","Burger","Pasta","Lahmacun","Salad"};
 	int pointArr[5];
 	int temp;
 
+	printf("ROUND 1 STARTED\n\n");
 	printf("Enter how many people you are: ");
-	scanf("%d", &count);
-	printf("Make 5 choices in order of 1-Kebab,2-Burger,3-Pasta,4-Lahmacun,5-Salad or others\n");
+	scanf("%d", &countOfPeople);
+	printf("Make 5 choices in order of\n1-Kebab\n2-Burger\n3-Pasta\n4-Lahmacun\n5-Salad\n");
 	printf("So, please start type your decisions one-by-one\n");
 
-	for(int i = 0; i < count; i++){
+	for(int i = 0; i < countOfPeople; i++){
+		printf("%dth person started selection\n",(i+1));
 		for(int j = 0; j < 5; j++){
+			printf("Enter the id of meal and #%d preference is: ",j+1);
 			scanf("%d", &temp);
 			switch (j){
 				case 0: pointArr[temp-1] += 5; break;
@@ -25,23 +29,44 @@ int main(){
 				case 4: pointArr[temp-1] += 1; break;
 			}
 		}
-		printf("%dth person finished selection\n",(i+1));
+		printf("%dth person finished selection\n\n",(i+1));
 	}
 
-    temp = 0;
+	char shortList[5][15];
+    int shortCount = 0;
+
+	printf("Threshold is %d and meals' total points :\n", THRESHOLD);
     for(int i = 0; i < 5; i++){
+		printf("%s:%d ",foodArr[i],pointArr[i]);
         if(pointArr[i]>THRESHOLD){
-            printf("%s:%d ",foodArr[i],pointArr[i]);
-            temp++;
+			strcpy(shortList[shortCount], foodArr[i]);
+            shortCount++;
         }
     }
     printf("\n");
 
-    if(temp==0){
+    if(shortCount==0){
         printf("You are eating at home/dorm today!\n");
         exit(0);
     }
 
+	int secondPointArr[shortCount];
+	
+	printf("\nROUND 2 STARTED\n\n");
+    printf("Please make new selections in short list\n");
 
+	for(int i=0; i < shortCount; i++){
+		printf("%d-%s\n",i+1,shortList[i]);
+	}
+	for(int i = 0; i < countOfPeople; i++){
+		printf("%dth person started selection\n",(i+1));
+		for(int j = 0; j < shortCount; j++){
+			printf("Enter the id of meal and #%d preference is: ",j+1);
+			scanf("%d", &temp);
+			secondPointArr[temp-1] += shortCount-j;
+			}
+		printf("%dth person finished selection\n\n",(i+1));
+	}
+	
 	return 0;
 }
